fae496125158f0d581412536fccc93f16147459d
Gustaf Starhög <gustafstar@hotmail.com>
2023-03-24 12:36:40 +0000

    added som const variabels to the js code for the different sections of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+// Variabels
+
+const nextBtn = document.getElementById("next-btn");
+const usernameInput = document.getElementById("username");
+const welcomeSection = document.getElementsByClassName("welcome-game-area");
+const difficultySection = document.getElementsByClassName("difficulty-game-area");
+const questionsSection = document.getElementsByClassName("questions-game-area");
+const finishedSection = document.getElementsByClassName("finished-game-area");
+const rulesSection = document.getElementsByClassName("game-rules-section");
+
+
+
+
+
+
+
 document.getElementById("username").focus();
