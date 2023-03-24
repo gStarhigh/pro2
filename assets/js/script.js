970be575f8c40d057502d556cb181843927ffe00
Gustaf Starhög <gustafstar@hotmail.com>
2023-03-24 13:13:32 +0000

    added IDs instead to all sections and adjusted the javascript to target the ids instead of class names

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 
 const startBtn = document.getElementById("start-btn");
 const usernameInput = document.getElementById("username");
-const welcomeSection = document.getElementsByClassName("welcome-game-area");
-const difficultySection = document.getElementsByClassName("difficulty-game-area");
-const questionsSection = document.getElementsByClassName("questions-game-area");
-const finishedSection = document.getElementsByClassName("finished-game-area");
-const rulesSection = document.getElementsByClassName("game-rules-section");
+const welcomeSection = document.getElementsById("welcome-game-area");
+const difficultySection = document.getElementsById("difficulty-game-area");
+const questionsSection = document.getElementsById("questions-game-area");
+const finishedSection = document.getElementsById("finished-game-area");
+const rulesSection = document.getElementsById("game-rules-section");
 const easyButton = document.getElementById("easy-btn");
 const mediumButton = document.getElementById("medium-btn");
 const hardButton = document.getElementById("hard-btn");
