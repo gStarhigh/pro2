e085d18a6790036db19f3284a9c510603cd5b93e
Gustaf Starhög <gustafstar@hotmail.com>
2023-03-24 13:10:18 +0000

    added const variabels to the different difficulty buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 // Variabels
 
-const nextBtn = document.getElementById("next-btn");
+const startBtn = document.getElementById("start-btn");
 const usernameInput = document.getElementById("username");
 const welcomeSection = document.getElementsByClassName("welcome-game-area");
 const difficultySection = document.getElementsByClassName("difficulty-game-area");
 const questionsSection = document.getElementsByClassName("questions-game-area");
 const finishedSection = document.getElementsByClassName("finished-game-area");
 const rulesSection = document.getElementsByClassName("game-rules-section");
+const easyButton = document.getElementById("easy-btn");
+const mediumButton = document.getElementById("medium-btn");
+const hardButton = document.getElementById("hard-btn");
 
 
 
