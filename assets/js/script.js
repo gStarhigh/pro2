a1775d922b63d91837f370dbc0c05b3029acb045
Gustaf Starhög <gustafstar@hotmail.com>
2023-03-24 14:41:28 +0000

    removed the hide class from all sections, removed an S from the get elementbyID const, and added JS to hide the sections that are not the welcome section and added a eventlistener to the start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 
 const startBtn = document.getElementById("start-btn");
 const usernameInput = document.getElementById("username");
-const welcomeSection = document.getElementsById("welcome-game-area");
-const difficultySection = document.getElementsById("difficulty-game-area");
-const questionsSection = document.getElementsById("questions-game-area");
-const finishedSection = document.getElementsById("finished-game-area");
-const rulesSection = document.getElementsById("game-rules-section");
+const welcomeSection = document.getElementById("welcome-game-area");
+const difficultySection = document.getElementById("difficulty-game-area");
+const questionsSection = document.getElementById("questions-game-area");
+const finishedSection = document.getElementById("finished-game-area");
+const rulesSection = document.getElementById("game-rules-section");
 const easyButton = document.getElementById("easy-btn");
 const mediumButton = document.getElementById("medium-btn");
 const hardButton = document.getElementById("hard-btn");
@@ -18,3 +18,25 @@ const nextQuestionButton = document.getElementById("next-question");
 
 // Focus on the username box when the page is loaded
 document.getElementById("username").focus();
+
+
+// Adds the hide class to all sections except the welcome section
+difficultySection.classList.add("hide");
+questionsSection.classList.add("hide");
+finishedSection.classList.add("hide");
+rulesSection.classList.add("hide");
+
+//Removes the hide class from the welcome section
+welcomeSection.classList.remove("hide");
+
+
+/**
+ * Listens after a click from the user on the start button, then removes the hide
+ * element from the difficulty section and adds it to the welcome section
+ */
+startBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    welcomeSection.classList.add("hide");
+    difficultySection.classList.remove("hide");
+});
